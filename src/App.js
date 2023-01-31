c348fa096541d6163aaabdfe31ee92c9ad91961b
tranvanhieu20032001 <tranvanhieu20032001@gmail.com>
2023-02-01 01:04:52 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Aboutme from "./components/Aboutme";
+import Contactme from "./components/Contactme";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import LinkBar from "./components/LinkBar";
+import NavBar from "./components/NavBar";
+import Project from "./components/Project";
+import Styles from "./components/Styles";
+import ScrolltoTop from "./components/ScrolltoTop";
+import app from './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className=" dark:bg-gray-700 bg-color-bg">
+    <NavBar/>
+    <Home/>
+    <LinkBar/>
+    <Aboutme/>
+    <Experience/>
+    <Project/>
+    <Contactme/>
+    <Styles/>
+    <ScrolltoTop/>
+  </div>
   );
 }
 
